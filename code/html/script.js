bc450a6b3483c1c04ed541ad780e9919018116b5
Harry England <125908088+H-England@users.noreply.github.com>
2025-03-12 19:00:11 +0000

    Added additional functionality to the dark mode button.
    The dark mode button now switches between ☀️ and 🌙 dependent on the mode you are in.

--- code/html/script.js
+++ code/html/script.js
@@ -2,16 +2,26 @@ document.addEventListener('DOMContentLoaded', function() {
  const darkModeToggle = document.getElementById('dark-mode-toggle');
  const body = document.body;
 
+ function updateButtonIcon() {
+    darkModeToggle.textContent = body.classList.contains('dark-mode') ? "☀️" : "🌙"
+ }
+
  // Check for saved user preference, if any, and apply it
  const userPrefersDark = localStorage.getItem('darkMode') === 'true';
  if (userPrefersDark) {
      body.classList.add('dark-mode');
  }
 
+ updateButtonIcon();
+
  darkModeToggle.addEventListener('click', function() {
      body.classList.toggle('dark-mode');
 
      // Save the user preference to local storage
-     localStorage.setItem('darkMode', body.classList.contains('dark-mode'));
+     const isDarkMode = body.classList.contains('dark-mode');
+     localStorage.setItem('darkMode', isDarkMode)
+
+     updateButtonIcon();
  });
 });
+
